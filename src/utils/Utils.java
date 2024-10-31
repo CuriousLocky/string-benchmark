af6f893a92196e03b354d7dfacdd2078542275fe
CuriousLocky <835322750jm37@outlook.com>
2024-10-31 10:19:11 +0100

    Change utils structure

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -1,4 +1,15 @@
 package utils;
 
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.util.Random;
+
 public class Utils {
+    public static String makeRandomString(int stringSize, Random rand) {
+        char[] chars = new char[stringSize];
+        for (int i = 0; i < stringSize; i++) {
+            chars[i] = (char) ((32 + rand.nextInt()) % 127);
+        }
+        return new String(chars);
+    }
 }
